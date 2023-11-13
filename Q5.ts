9e06c801daeea1d06a83329a8244030a1cdd57e2
TechSavvy-Ree <ranya7476@gmail.com>
2023-11-13 08:47:55 -0800

    Q5;Store author name and compose message

--- Q5.ts
+++ Q5.ts
@@ -3,3 +3,4 @@ let person: string = "Albert Einstein";
 let message: string =
     "A person who never made a mistake never tried anything new.";
   console.log("${person},  ${message}");
+//  Intialize two variable s 'Person' & 'message, then print 
