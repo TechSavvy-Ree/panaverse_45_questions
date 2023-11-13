e154555cdcdde0fd1cb27e954e42933bd9f378cf
TechSavvy-Ree <ranya7476@gmail.com>
2023-11-13 07:43:50 -0800

    Q2; personal Message

---
+++ Q5.ts
@@ -0,0 +1,5 @@
+//q5 Store author name in a variable and then compose message and store it in a New varible.
+let famous_person: string = "Albert Einstein";
+let message: string =
+    "A person who never made a mistake never tried anything new.";
+  console.log(`${famous_person},  ${message}`);
