b88a592ad5d02984e1d427d14ebdb6f172e58dee
TechSavvy-Ree <ranya7476@gmail.com>
2023-11-13 08:28:17 -0800

    Favourite number and message

---
+++ Q9.ts
@@ -0,0 +1,4 @@
+//q9: print favourite number with message.
+let  favorite_number: number = 16;
+let favorite_number_message: string = `My favorite number is ${favorite_number} `;
+console.log(favorite_number_message);
