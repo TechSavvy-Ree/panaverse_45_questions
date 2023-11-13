b1edcb2f01e13e5ab8e0e4ea06da32c771b6f023
TechSavvy-Ree <ranya7476@gmail.com>
2023-11-13 08:35:24 -0800

    Q9; Favourite number and message

--- Q9.ts
+++ Q9.ts
@@ -2,3 +2,5 @@
 let  favorite_number: number = 16;
 let favorite_number_message: string = `My favorite number is ${favorite_number} `;
 console.log(favorite_number_message);
+/* Declare and initialize two variables, give type number and string,
+then store number and message in these variables and print */ 
