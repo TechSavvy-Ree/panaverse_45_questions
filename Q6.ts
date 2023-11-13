d24170920851f477291527ef5000b341dc0cceba
TechSavvy-Ree <ranya7476@gmail.com>
2023-11-13 08:23:16 -0800

    Q6; Stripping Names

---
+++ Q6.ts
@@ -0,0 +1,5 @@
+// q6 stripping names: 
+let stripping: string = "Tech \t  Savvy  \n   is  \t developer ";
+console.log(stripping);
+// '\t' is replaced by  tab character.
+// '\n' causes a new line, moving the text 'is' to next line
