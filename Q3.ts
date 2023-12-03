e84adb232a44f591ec0d5cd9997f741a2bf11b18
TechSavvy-Ree <ranya7476@gmail.com>
2023-12-02 18:43:59 -1200

    Name cases: Lower case,Upper case and Title case

---
+++ Q3.ts
@@ -0,0 +1,21 @@
+//q3 
+//Name cases: To lower case,Upper case &Title case
+let Name: string = "Eric lious";
+// for lower case: 
+console.log(
+    `Hello ${Name.toLowerCase()}, would you like to learn some Python today?`
+);
+// for UPPER case:
+console.log(
+    `Hello ${Name.toUpperCase()}, would you like to learn some Python today?`
+);
+// For Title case:
+const subName:Array<string>=Name.split(" ")
+ console.log(subName)
+ const modifyarry=subName.map((value)=>{
+  const temp=value[0].toUpperCase();
+  console.log(temp);
+  return temp + value.slice(1)
+ })
+ console.log(modifyarry.join(" "))
+ export{}
