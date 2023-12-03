24211ec9806c61a822fd63c8d9ac9682c88f4469
TechSavvy-Ree <ranya7476@gmail.com>
2023-12-02 18:58:17 -1200

    Q8: number 8 appearing once on each line

---
+++ Q8.ts
@@ -0,0 +1,9 @@
+//q8: Four lines with the number 8 appearing once on each line
+console.log( 5 + 3);
+console.log("_________________");
+console.log(16 - 8);
+console.log("_________________");
+console.log(4 * 2);
+console.log("_________________");
+console.log(64 / 8);
+console.log("_________________");
