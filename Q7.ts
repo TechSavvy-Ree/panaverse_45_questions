6d3213406e1cf6bf27bf5199b918126552c6bbd2
TechSavvy-Ree <ranya7476@gmail.com>
2023-12-02 18:55:44 -1200

    Using Arithmetic operators to get answer 8

---
+++ Q7.ts
@@ -0,0 +1,6 @@
+//q7: Apply Addition,Subtraction,Multiplication & Division operator to get Ans. 8
+// Using simple Arithmetic operators to get answer 8 
+console.log(5 + 3);
+console.log(16 - 8);
+console.log(4 * 2);
+console.log(64 / 8);
