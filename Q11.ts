757c23dc9ca7f5332000f81dc13bd7ddf7a86030
TechSavvy-Ree <ranya7476@gmail.com>
2024-02-14 13:20:51 -1200

    Q10:Adding comments

---
+++ Q11.ts
@@ -0,0 +1,17 @@
+// 11. Names: Store the names of a few of your friends in an array called names. 
+// Print each person’s name by accessing each element in the list, one at a time.
+
+// Index starts with 0;      0        1           2         3
+      let names:string[]= ["Smith", "Robert", "Juleyat", "Eric"];
+console.log(names);     // Accessing all elements
+
+
+//Accessing each element  of Array  one by one.
+
+console.log(names[0]); //Print 1st element , Smith
+console.log(names[1]); //Print 2nd element , Robert
+console.log(names[2]); //Print 3rd element , juleyat
+console.log(names[3]); //Print 4th element , Eric
+console.log(names[4]); // Return undefined
+
+export{}
---
+++ Q12.ts
@@ -0,0 +1,13 @@
+// 12. Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them.
+//  The text of each message should be the same, but each message should be personalized with the person’s name.
+
+let names:string[]= ["Smith", "Robert", "Juleyat", "Eric"];
+
+console.log(`Hello ${names[0]}!`); //Print 1st element , Smith
+console.log(`Hi!  ${names[1]}`); //Print 2nd element , Robert
+console.log(`Hy! what's up ${names[2]}?`); //Print 3rd element , juleyat
+// console.log(`Hello  ${names[3]}`); //Print 4th element , Eric
+
+
+
+console.log(`Hello ${names[0]} and ${names[3]}!`); 
