625be234c2cf90f02f46326c4604040bb48a4dc8
TechSavvy-Ree <ranya7476@gmail.com>
2024-02-14 13:26:38 -1200

    Q12: Personalized message with each person’s name

--- Q12.ts
+++ Q12.ts
@@ -1,7 +1,7 @@
 // 12. Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them.
 //  The text of each message should be the same, but each message should be personalized with the person’s name.
 
-let names:string[]= ["Smith", "Robert", "Juleyat", "Eric"];
+let names:string[]= ["Smith", "Robert", "juleyat", "Eric"];
 
 console.log(`Hello ${names[0]}!`); //Print 1st element , Smith
 console.log(`Hi!  ${names[1]}`); //Print 2nd element , Robert
