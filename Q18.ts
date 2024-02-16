2fa3d39ef5b2dc1b22b600a593c423f7a8fff803
TechSavvy-Ree <ranya7476@gmail.com>
2024-02-16 07:08:01 -1200

    Q6: stripping Names

---
+++ Q18.ts
@@ -0,0 +1,59 @@
+/* Q18: Seeing the World: Think of at least five places in the world you’d like to visit.
+*/
+// Step-1: Printing a simple Array
+let locationsList: string[] = [
+    "Saudia Arabia",
+    "Paris",
+    "Tokyo",
+    "Iraq", 
+    "America"
+  ];
+// Step-2:
+console.log("Original Array"); 
+console.log(`----------------------`);
+  locationsList.map((location: string) => {
+  console.log(location);
+  });
+
+  console.log(`----------------------`)
+// Step-3:
+  locationsList.sort().map((location: string) => {
+   console.log(location);
+  });
+  console.log(`----------------------`)
+// Step-4:
+  locationsList.map((location: string) => {
+    console.log(location);
+  });
+  console.log(`----------------------`)
+// Step-5
+  locationsList.reverse().map((location: string) => {
+    console.log(location);
+  });
+  console.log(`----------------------`)
+
+  locationsList.map((location: string) => {
+    console.log(location);
+  });
+  console.log(`----------------------`)
+
+  locationsList.reverse();
+  locationsList.map((location: string) => {
+    console.log(location);
+  });
+  console.log(`----------------------`)
+
+  locationsList.reverse();
+  locationsList.map((location: string) => {
+    console.log(location);
+  });
+  console.log(`----------------------`)
+  locationsList.sort();
+  locationsList.map((location: string) => {
+    console.log(location);
+  });
+  console.log(`----------------------`)
+  locationsList.reverse().sort();
+  locationsList.map((location: string) => {
+    console.log(location);
+  })
--- Q6.ts
+++ Q6.ts
@@ -1,5 +1,9 @@
 // q6 stripping names: 
-let stripping: string = "Tech \t  Savvy  \n   is  \t developer ";
+let stripping: string = " \t Tech Savvy  \n is developer \t";
 console.log(stripping);
 // '\t' is replaced by  tab character.
 // '\n' causes a new line, moving the text 'is' to next line
+
+//after trim
+console.log(stripping.trim());
+export{}
