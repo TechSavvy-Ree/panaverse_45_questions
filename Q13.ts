aada3c3379a411f8890efc8cb0ba232a5ba5ae0e
TechSavvy-Ree <ranya7476@gmail.com>
2024-02-17 07:59:36 -1200

    Q13:Print a series of statements about the items of Array

--- Q13.ts
+++ Q13.ts
@@ -2,7 +2,11 @@
 //Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”
 
 const vehicle_types: Array<string> = ["Bicycle","Motorcycle", "car",];
+for(let item of vehicle_types){
+console.log(`I would like to own a ${item}.`);
+}
 
-vehicle_types.map((item: string) => {
-  console.log(`I would like to own a ${item}.`);
-});
+// -------------------another method to access the Array's element--
+// vehicle_types.map((item: string) => {
+//     console.log(`I would like to own a ${item}.`);
+// });
