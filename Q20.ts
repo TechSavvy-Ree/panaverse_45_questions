dab47ce9f08a7a25e098b9db48e74fdffe58d731
TechSavvy-Ree <ranya7476@gmail.com>
2024-02-17 08:29:58 -1200

    Q20:List of items store in an array

---
+++ Q20.ts
@@ -0,0 +1,10 @@
+/*Think of something you could store in a array.Write a program that creates a list containing these items. */
+// Define an array of fruit names
+let fruits: string[] = ["Apple", "Banana", "Orange", "Grapes", "Watermelon"];
+
+// Print out each fruit in the array
+console.log("List of fruits:");
+fruits.forEach(fruit => {
+    console.log(fruit);
+});
+
