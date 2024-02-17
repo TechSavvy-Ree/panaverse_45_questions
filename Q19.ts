7efd1bf8dbcf4242ca1cfbaa6101beffe6608fea
TechSavvy-Ree <ranya7476@gmail.com>
2024-02-17 08:13:34 -1200

    Q19:indicating the number

--- Q19.ts
+++ Q19.ts
@@ -1,5 +1,5 @@
 /*  Dinner Guests:print a message indicating the number
 of people you are inviting to dinner.*/
 let guestList: Array<string> = ["Eric", "stamen","Juleyat","Robert"];
-console.log(`${guestList.length} people are inviting`);
+console.log(`${guestList.length} people are inviting for dinner`);
 export{}
