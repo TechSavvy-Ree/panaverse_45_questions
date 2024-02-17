10e30aedc786e3435f52ff45f5b7ebc6da870441
TechSavvy-Ree <ranya7476@gmail.com>
2024-02-17 09:24:28 -1200

    Q20:Creates object & access its properties

---
+++ Q21.ts
@@ -0,0 +1,18 @@
+/* They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items. */
+let cars:{
+    brand:string;
+    model:string;
+    color:string;
+    year:number;
+}
+let car1={
+   brand:"Honda",
+   model:"Civic",
+   year:2005,
+   color:"White"
+}
+console.log("Information about cars:");
+console.log(car1);
+car1.color="green";
+console.log(car1);
+
