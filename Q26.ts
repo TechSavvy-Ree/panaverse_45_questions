3686d1c186846051d2377ede94174d39d2327b1c
TechSavvy-Ree <ranya7476@gmail.com>
2024-02-22 20:48:07 -1200

    Q26:Check Alien color,that runs the if block and another that runs the else block.

---
+++ Q26.ts
@@ -0,0 +1,17 @@
+/*Alien Colors #2: Choose a color for an alien as you did in Exercise 25, and
+write an if-else chain. */
+let alien_color2: string = 'green';
+// In this version, If block is run.
+
+if (alien_color2 === 'green') {
+    console.log("Player just earned 5 points.");
+} else {
+    console.log("Player just earned 10 points");
+}
+// In this block, Else statement is print. 
+// alien_color2='yellow';
+if (alien_color2 === 'Red') {
+    console.log("Player just earned 5 points.");
+} else {
+    console.log("Player just earned 10 points");
+}
