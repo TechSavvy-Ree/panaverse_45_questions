d5cb096e2abcbdce6436677da35bbeed144016dd
TechSavvy-Ree <ranya7476@gmail.com>
2024-02-25 22:15:47 -1200

    Q42:Magician's name with message

---
+++ Q42.ts
@@ -0,0 +1,22 @@
+/*reat Magicians: Start with a copy of your program from Exercise 39.
+Write a function called make_great() that modifies the array of magicians by adding
+the phrase the Great to each magician’s name. Call show_magicians() to
+see that the list has actually been modified.*/
+let show_magicians=(magicians: string[]) => {
+    magicians.forEach(magician => {
+        console.log(magician);
+    });
+}
+
+function make_great(magicians: string[]): void {
+    for (let i = 0; i < magicians.length; i++) {
+        magicians[i] = "the Great " + magicians[i];
+    }
+}
+
+const magiciansArray: string[] = ["Merlin", "Harry Houdini", "David Copperfield", "Penn & Teller"];
+
+make_great(magiciansArray);
+show_magicians(magiciansArray);
+
+export{}
